e281fdbf21dc609eca55498146544ca1c4979241
Sameer Pokharel <i.amsameer2061@gmail.com>
2021-08-17 09:51:00 +0545

    build hearder section for landing screen

---
+++ lib/widgets/netflix-logo.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_builder/responsive_builder.dart';
+
+class NetflixLogo extends StatelessWidget {
+  const NetflixLogo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenTypeLayout(
+        mobile: _image(30), tablet: _image(40), desktop: _image(40));
+  }
+
+  Widget _image(double size) {
+    return Container(
+      height: size,
+      child: Image.asset('assets/images/netflix-logo-full.png'),
+    );
+  }
+}
